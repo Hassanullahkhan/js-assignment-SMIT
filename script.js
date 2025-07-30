ce77323dc4146fb779fa5efc5adaef4bab5a31a2
Hassan Khan <hasaankhaan456@gmail.com>
2025-07-30 07:23:48 +0500

    js-assignment-SMIT

---
+++ script.js
@@ -0,0 +1,191 @@
+
+// Alerts
+
+// Exercises1. 
+// alert("Hello, World");
+
+// Exercises2. 
+// alert("Hassan Khan");
+
+// Exercises3. 
+// alert(4);
+
+// Exercises4.
+// var message = "Hello how are you";
+// alert(message);
+
+// Exercises5.
+// let greetings = "Hello" + " how are you";
+// alert(greetings);
+
+
+
+// Variables for Storing Data: Declaring and Using Text Data
+
+
+
+// Exercise1.
+
+// var userName = "Hassan Khan";
+// console.log(userName);
+
+
+// Exercise2.
+
+// var welcome = "welcome to JS";
+// alert(welcome);
+
+// Exercise3.
+
+// var fullName = "Hassan " + "Khan";
+// console.log(fullName);
+
+// Exercise4.
+
+// var quote = "Kamyabi ka age se koi relation nahi hai"
+// console.log(quote);
+
+// Exercise5.
+
+// var city = "Karachi";
+// alert(`welcome to ${city} `);
+
+// Working with NUmbers and Basic Arithmetic
+
+// 3. Variables for Numbers: Storing and Using Numerical Data
+
+
+// Exercise1.
+
+// let age = 22;
+
+// Exercise2.
+
+// let num1 = 3;
+// let num2 = 7;
+// let result= num1 + num2;
+// console.log(result);
+
+// Exercise3.
+
+// let num3 = 7;
+// let num4 = 3;
+// let result1 = num3 - num4;
+// console.log(result1);
+
+// Exercise4.
+
+// let multiply = num3 * num2;
+// console.log(multiply);
+
+// Exercise5.
+
+// alert(`you are ${age} years old`);
+
+
+
+//4. Math Expressions;
+
+
+// // Exercise1.
+//   let x = 10;
+//   let y = 5;
+//   let result = x + y;
+//   console.log(result);
+    
+
+// // Exercise2.
+//    let subtract = y - x;
+//    console.log(subtract);
+
+// // Exercise3.
+//     let multiply = x * y;
+//     console.log(multiply);
+    
+
+// // Exercise4.
+//     let divide = x / y;
+//     console.log(divide);
+    
+
+// // Exercise5.
+//     let remainder = x % y;
+//     console.log(remainder);
+    
+// Getting User Input and Conditional Logic
+
+// 5. Prompt: Getting Input from the User
+
+
+// // Exercise1.
+
+// let names = prompt("What is Your Name.");
+// alert("Hello " + names);
+
+// // // Exercise2.
+
+// let age = prompt("What is Your age.");
+// alert("Your age is " + age);
+
+// // // Exercise3.
+
+// let guess = prompt("Give me a number ");
+// let double = guess * guess;
+// alert("The double of the guess is " + double);
+
+// // // Exercise4.
+
+// let color = prompt("what is your favourite color");
+// alert("Wow I like " + color + " too");
+
+//  // Exercise5.
+
+//  let city = prompt("Which city do you live in");
+//  alert("You are from " + city);
+
+
+ // 6. if statement and Comparision Operators
+ 
+ 
+ //1. Exercise 1.
+
+ let age = prompt("What is your age");
+ if (age < 30) {
+    alert("Your still young man.");
+ }
+else if (age > 30) {
+    alert("Your are old.");
+}
+else {
+    alert("uncorrect input");
+}
+
+let evenNumber = prompt("Give me a number ");
+
+if((evenNumber / 2) === 0) {
+    alert("this is an even number");
+}
+
+else {
+    alert("this is not an even Number");
+}
+
+alert("number comparision Game!!!");
+
+let firstNumber = prompt("give me a number");
+let secondNumber = prompt("give me another number");
+
+if (firstNumber > secondNumber) {
+    alert("first number is greater then second Number")
+}
+else if (secondNumber > firstNumber) {
+    alert("second number is greater then first Number")
+}
+else if (firstNumber === secondNumber) {
+    alert("both numbers are equal");
+}
+
+else {
+        alert("incorrect input");
+  
+}
